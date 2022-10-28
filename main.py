8c43bbfec93bef974c9dc4e036a42a027926d9b1
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2022-10-28 12:07:32 +0200

    code cleanup

--- main.py
+++ main.py
@@ -1,55 +1,45 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-
+from test_graphs import graph1, graph2, graph3
 from base_genetic_algorithm import BaseGeneticAlgorithm
 from crossovers import OnePlaceCrossover
 from graph_coloring_algorithm import GraphVertexColoringPopulation
 from mutations import OnePlaceMutation
 from selections import RouletteSelection
+from tqdm import tqdm
 
 
-def to_adj_matrix(string: str):
-    lines = string.split('\n')
-    adj_mat = []
-    for line in lines:
-        line = line.strip()
-        line = [val.strip() for val in line.split(",")]
-        clear_line = []
-        for v in line:
-            if len(v) > 0:
-                clear_line.append(int(v))
-        adj_mat.append(clear_line)
-    return np.matrix(adj_mat)
+def show_graph_with_colors(graph, vertex_colors):
+    pos = nx.shell_layout(graph)
+    color_map = plt.cm.get_cmap('Spectral')
+
+    nx.draw(
+        graph,
+        pos,
+        node_color=[color_map(vertex_colors[idx] / max(vertex_colors)) for idx, node in enumerate(graph)],
+        with_labels=True
+    )
+    plt.show()
+
+
+def plot_fitness_in_time(algorithm_history, mean=False):
+    if not mean:
+        plt.plot([abs(pop.get_best_individual_fitness_score()) for pop in tqdm(algorithm_history)])
+    else:
+        plt.plot([abs(np.array(pop.get_fitness_for_all()).mean()) for pop in tqdm(algorithm_history)])
+    plt.show()
 
 
 def main():
-    graph = """0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 
-0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 
-1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 
-1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 
-0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 
-0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 
-1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 
-0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 
-0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 
-0, 0, 0, 1, 0, 1, 0, 0, 1, 0,  """
-    graph = to_adj_matrix(graph)
-    # graph = np.matrix(
-    #     [[0, 1, 0, 0, 1, 0, 0, 0],
-    #      [1, 0, 1, 1, 1, 0, 1, 0],
-    #      [0, 1, 0, 1, 0, 1, 1, 0],
-    #      [0, 1, 1, 0, 1, 0, 0, 1],
-    #      [1, 1, 0, 1, 0, 0, 0, 0],
-    #      [0, 0, 1, 0, 0, 0, 0, 0],
-    #      [0, 1, 1, 0, 0, 0, 0, 0],
-    #      [0, 0, 0, 1, 0, 0, 0, 0]]
-    # )
+    graph = graph3
+
+    G = nx.Graph(graph)
 
     gen_alg = BaseGeneticAlgorithm(
         population=GraphVertexColoringPopulation(10, graph),
         crossover=OnePlaceCrossover(probability=0.8),
-        mutation=OnePlaceMutation(probability=0.05),
+        mutation=OnePlaceMutation(probability=0.1),
         selection=RouletteSelection()
     )
     gen_alg.run(10000)
@@ -58,20 +48,9 @@ def main():
     print(gen_alg.population.get_fitness_for_individual(color_solution))
     print(len(set(color_solution)))
 
-    gr = nx.Graph(graph)
-    pos = nx.shell_layout(gr)
-    color_map = plt.cm.get_cmap('Spectral')
+    show_graph_with_colors(G, color_solution)
+    plot_fitness_in_time(gen_alg.history, True)
 
-    # nx.draw(
-    #     gr,
-    #     pos,
-    #     node_color=[color_map(color_solution[idx] / max(color_solution)) for idx, node in enumerate(gr)],
-    #     with_labels=True
-    # )
-    # plt.show()
-
-    plt.plot([abs(pop.get_best_individual_fitness_score()) for pop in gen_alg.history])
-    plt.show()
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,5 +1,18 @@
 from typing import List
+import numpy as np
 
+def to_adj_matrix(string: str):
+    lines = string.split('\n')
+    adj_mat = []
+    for line in lines:
+        line = line.strip()
+        line = [val.strip() for val in line.split(",")]
+        clear_line = []
+        for v in line:
+            if len(v) > 0:
+                clear_line.append(int(v))
+        adj_mat.append(clear_line)
+    return np.matrix(adj_mat)
 
 def is_list_of_unique_objects(l: List) -> bool:
     test = []
